040a7c4442a73308e151f5a436c5427bb8b8b9f2
HRKNK <nikita.zyamzin@yandex.ru>
2024-02-15 12:13:20 +0300

    Update

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-module.exports = (env) => {
+interface ENV {
+	mode: 'development' | 'production',
+	port: number,
+};
 
+export default (env: ENV) => {
 	// Логи окружения
-	console.log('Режим сборки:', env.mode, '\n'); 
+	console.log('Режим сборки:', env.mode ?? 'server', '\n');
 
-	return {
+	const config: webpack.Configuration = {
 		mode: env.mode ?? 'development', // режим сборки
 		entry: { // точка входа. объект-перечисление нескольких точек
 			bundle: path.resolve(__dirname, 'src', 'index.ts'), // где ключ - имя бандла на выходе (при отсутствии output конфигурации)
 		},
+
+		// конфигурация дэв-сервера
+		devServer: {
+			port: env.port ?? 3000,
+			open: true, // открыть браузер
+		},
+
 		output: { // куда собрать
 			filename: '[name].[contenthash].js', 
 			// имя бандла // [name] включает entry-ключ как имя бандла // [contenthash] включает хэш-файла в название файла
@@ -38,4 +50,6 @@ module.exports = (env) => {
 		},
 
 	}
+
+	return config;
 };
