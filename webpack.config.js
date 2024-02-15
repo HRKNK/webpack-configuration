5192e0e114c8b4badf842bf6798a079cbd8a4290
HRKNK <nikita.zyamzin@yandex.ru>
2024-02-15 11:11:39 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-	mode: 'production', // режим сборки
-	entry: { // точка входа. объект-перечисление нескольких точек
-		bundle: path.resolve(__dirname, 'src', 'index.js'), // где ключ - имя бандла на выходе (при отсутствии output конфигурации)
-	},
-	output: { // куда собрать
-		filename: '[name].[contenthash].js', 
-		// имя бандла // [name] включает entry-ключ как имя бандла // [contenthash] включает хэш-файла в название файла
-		path: path.resolve(__dirname, 'build'), // путь хранения бандла
-		clean: true, // при сборке очищает папку хранения бандла
-	},
+module.exports = (env) => {
+
+	// Логи окружения
+	console.log('Режим сборки:', env.mode, '\n'); 
+
+	return {
+		mode: env.mode ?? 'development', // режим сборки
+		entry: { // точка входа. объект-перечисление нескольких точек
+			bundle: path.resolve(__dirname, 'src', 'index.js'), // где ключ - имя бандла на выходе (при отсутствии output конфигурации)
+		},
+		output: { // куда собрать
+			filename: '[name].[contenthash].js', 
+			// имя бандла // [name] включает entry-ключ как имя бандла // [contenthash] включает хэш-файла в название файла
+			path: path.resolve(__dirname, 'build'), // путь хранения бандла
+			clean: true, // при сборке очищает папку хранения бандла
+		},
+		plugins: [ // массив плагинов
+			new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }), // шаблон index.html
+			new webpack.ProgressPlugin(), // процентный прогресс сборки (консоль)
+		],
+	}
 };
